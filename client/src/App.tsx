1c59d3e7222a687dedacd3d1a186efb1c7c90d6e
Deluxan Mariathasan <phoneme_capable0i@icloud.com>
2026-02-14 21:19:41 +0530

    Integrate material ui and material icons

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from '@mui/material'
+import AcUnitIcon from '@mui/icons-material/AcUnit'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,6 +30,10 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Button variant="contained" color="error">
+        Button
+      </Button>
+      <AcUnitIcon />
     </>
   )
 }
